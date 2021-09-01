65a977f47174d4a0366efed3964aa4e515d093fb
biaaimi <biagioni.aimi@gmail.com>
2021-09-01 20:55:45 -0300

    leet

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tradutor;
+         Console.Write("texto a ser traduzido: ");
+         
+         tradutor= Console.ReadLine();
+         Console.WriteLine(tradutor 
+
+             .Replace("A", "4")
+             .Replace("a", "4")
+             .Replace("E", "3")
+             .Replace("e", "3")
+             .Replace("I", "1")
+             .Replace("i", "1")
+             .Replace("L", "1")
+             .Replace("l", "1")
+             .Replace("O", "0")
+             .Replace("o", "0")
+             .Replace("T", "7")
+             .Replace("t", "7")
+             .Replace("S", "5")
+             .Replace("s", "5"));
+
+             Console.WriteLine(" pressione ENTER para sair");
+             Console.ReadKey(
+
+
+         );
+
+            
+        }
+    }
+}
